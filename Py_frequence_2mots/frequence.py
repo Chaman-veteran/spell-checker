939df2073234f33a3dea503c2b9ebdb522974d30
Chaman Vétéran <nathm.va@gmail.com>
2022-10-26 13:24:46 +0200

    ajout Harry Potter tome 4

--- Py_frequence_2mots/frequence.py
+++ Py_frequence_2mots/frequence.py
@@ -5,13 +5,17 @@ files = os.path.abspath(".")+"\\"
 f1 = open(files+"Harry_Potter_1.txt", 'r')
 f2 = open(files+"Harry_Potter_2.txt", 'r')
 f3 = open(files+"Harry_Potter_3.txt", 'r')
+f4 = open(files+"Harry_Potter_4.txt", 'r')
 data = f1.read().lower().split(' ') + f2.read().lower().split(' ') \
-        + f3.read().lower().split(' ')
+        + f3.read().lower().split(' ') + f4.read().lower().split(' ')
 f1.close()
 f2.close()
 f3.close()
+f4.close()
 dico = {}
-# dico :: {String : {String : Int}} 
+freq = {}
+# dico :: {String : {String : Int}} pour l'association
+# freq :: {String : Int} pour les mots les plus fréquents
 for ind in range(len(data)-1):
     nextWord = data[ind+1]
     currentWord = data[ind]
@@ -23,16 +27,14 @@ for ind in range(len(data)-1):
             dico[currentWord][nextWord] = 1
     else:
         dico[currentWord] = {nextWord : 1}
-
-len(dico)
+    if currentWord in freq:
+        freq[currentWord] += 1
+    else:
+        freq[currentWord] = 1
 
 for word in dico:
     dico[word] = dict(sorted(dico[word].items(), key=lambda y: y[1], reverse=True)[:7])
 
-#for word in dico:
-#    dico[word] = json.dumps(dico[word], indent=4)
-#print(json.dumps(dico, indent=4)[:100])
-
-f = open("D:/Users/Napp/Documents/Mes_documents/Informatique/Haskell/correcteur/assoc.txt",'w')
+f = open(files+"assoc.txt",'w')
 f.write(json.dumps(dico, indent=4))
 f.close()
