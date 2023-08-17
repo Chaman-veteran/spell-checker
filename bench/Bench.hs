3255500f63e0b5a139f0aa22ebc17c21ac5e090a
Chaman-veteran <nathm.va@gmail.com>
2023-08-17 21:53:28 +0200

    changed bench from wnf to nf

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -4,8 +4,8 @@ module Main where
 import Data.Aeson (FromJSON, decodeStrict, parseJSON, withObject, (.:))
 import Data.ByteString.Char8 (pack)
 import Data.Maybe (mapMaybe)
-import Control.DeepSeq (NFData(..))
-import Criterion.Main (defaultMain, bench, bgroup, env, whnf) 
+import Control.DeepSeq (NFData(..), force)
+import Criterion.Main (defaultMain, bench, bgroup, env, nfIO, nf) 
 
 import Data.WordTree
 
@@ -16,10 +16,13 @@ instance FromJSON CountedWord where
       <*> v .: "properties"
 
 instance NFData WordProperties where
-    rnf (WordProperties f i) = rnf f `seq` rnf i
+  rnf (WordProperties frequency info) = rnf frequency `seq` rnf info
+
+instance NFData CountedWord where
+  rnf (CountedWord word properties) = rnf word `seq` rnf properties  
 
 instance NFData a => NFData (Tree a) where
-    rnf (Node p b) = rnf p `seq` rnf b
+  rnf (Node properties branches) = rnf properties `seq` rnf branches
 
 setupEnv :: IO (Tree Char)
 setupEnv = do
@@ -30,10 +33,13 @@ setupEnv = do
 
 main :: IO ()
 main = defaultMain [
+  bgroup "Tree" [
+    bench "Tree" $ nfIO setupEnv
+  ],
   env setupEnv $ \dictionaryTree -> bgroup "Completion" [
-    bench "Completion" $ whnf (`giveSuffixe` "") dictionaryTree
+    bench "Completion" $ nf (`giveSuffixe` "") dictionaryTree
   ],
   env setupEnv $ \dictionaryTree -> bgroup "Correction" [
-    bench "Correction" $ whnf (\d -> similarWords d 4 "Haskell") dictionaryTree 
+    bench "Correction" $ nf (\d -> similarWords d 3 "Haskell") dictionaryTree 
   ]
   ]
