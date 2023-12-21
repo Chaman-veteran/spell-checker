4145355dd87cc479d5bdd66fa16587734cd5559b
Chaman Vétéran <nathm.va@gmail.com>
2023-12-21 13:41:56 +0100

    StatsFromTxt now uses Text strings

--- src/StatsFromTxt.hs
+++ src/StatsFromTxt.hs
@@ -11,15 +11,20 @@
 
 module Main (main) where
 
-import System.IO (readFile, writeFile)
+import System.IO (writeFile)
 import System.Directory (listDirectory)
-import qualified Data.Map.Strict as M
-  (Map, insert, empty, insertWith, foldrWithKey, map)
-import Control.Monad (forM)
+
 import Data.List (insert, find, sortOn)
 import Data.Maybe (maybe)
 import Data.Bifunctor (second)
+import Data.Text (Text)
+import qualified Data.Text.IO as TIO (readFile)
+import qualified Data.Text as T
+import qualified Data.Map.Strict as M
+  (Map, insert, empty, insertWith, foldrWithKey, map)
+
 import Codec.Serialise (writeFileSerialise)
+import Control.Monad (forM)
 import Options.Generic (getRecord, ParseRecord, Generic, type (<?>))
 
 -- Should we serialize the Tree instead of the Map? --
@@ -36,42 +41,42 @@ main = do
   serializeMap lang
 
 -- | Fetch words from a file
-getWords :: FilePath -> IO [String]
-getWords file = words <$> readFile file
+getWords :: FilePath -> IO [Text]
+getWords file = T.words <$> TIO.readFile file
 
 -- | Assemble all words from different dictionaries
-getFiles :: String -> IO [String]
+getFiles :: String -> IO [Text]
 getFiles lang = do
-  let dictionaryFiles = "Dictionaries//"++lang
-  files <- map ((dictionaryFiles++"//") ++) <$> listDirectory dictionaryFiles
+  let dictionaryFiles = "Dictionaries//" ++ lang
+  files <- map ((dictionaryFiles ++ "//") ++) <$> listDirectory dictionaryFiles
   wordsPerFile <- forM files getWords
   return $ concat wordsPerFile
 
 -- | Merge two same words by suming the frequencies and following words 
-addValue :: (Int, [(String,Int)]) -> (Int, [(String,Int)]) -> (Int, [(String,Int)])
+addValue :: (Int, [(Text,Int)]) -> (Int, [(Text,Int)]) -> (Int, [(Text,Int)])
 addValue (incFreq, [(nextWord,_)]) (freq, words) = (freq+incFreq, insert (nextWord, pNext) words)
   where pNext = 1 + maybe 0 snd (find (\w -> fst w == nextWord) words)
 
 -- | Map a word to his frequence and the next words with the probabilities associated
-getFreqnNext :: [String] -> M.Map String (Int, [(String,Int)])
+getFreqnNext :: [Text] -> M.Map Text (Int, [(Text,Int)])
 getFreqnNext [] = M.empty
 getFreqnNext [word] = M.insertWith addValue word (1, [(".",1)]) M.empty
 getFreqnNext (w:ws) = M.insertWith addValue w (1, [(head ws,1)]) $ getFreqnNext ws
 
 -- | Function to get the following words in sorted order
-getNextsSorted :: M.Map String (Int, [(String, Int)]) -> M.Map String (Int, [String])
+getNextsSorted :: M.Map Text (Int, [(Text, Int)]) -> M.Map Text (Int, [Text])
 getNextsSorted = M.map $ second (map fst . sortOn snd)
 
 -- | Fetch statistics as a Map object
-getStatsFromFile :: String -> IO (M.Map String (Int, [String]))
+getStatsFromFile :: String -> IO (M.Map Text (Int, [Text]))
 getStatsFromFile lang = getNextsSorted.getFreqnNext <$> getFiles lang
 
--- | Transforms a Map storing statistics of words to stringified JSON
-mapToStr :: M.Map String (Int, [String]) -> String
-mapToStr = M.foldrWithKey (\key value str -> translateWord key value ++ str) ""
-  where translateWord word (freq, nextWords) = "{\"word\":"++ show word
-                                                  ++",\"properties\":["++show freq
-                                                  ++","++show (take 3 nextWords)++"]} "
+-- | Transforms a Map storing statistics of words to Textified JSON
+mapToStr :: M.Map Text (Int, [Text]) -> Text
+mapToStr = M.foldrWithKey (\key value str -> translateWord key value `T.append` str) ""
+  where translateWord word (freq, nextWords) = "{\"word\":" `T.append` word `T.append`
+                                                  T.pack (",\"properties\":[" ++ show freq
+                                                        ++ ',' : (show (take 3 nextWords) ++ "]} "))
 
 -- | Serialize the map associating words to their properties in a file
 serializeMap :: String -> IO ()
